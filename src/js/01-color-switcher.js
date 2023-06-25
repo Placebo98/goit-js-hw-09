356b16bc8619a17c7cd737e2dd233d0fcd4b7eba
Placebo98 <oleg.tsaryk98@gmail.com>
2023-06-26 00:59:48 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
+let timerId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+btnStart.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+    btnStart.setAttribute('disabled', false);
+  }, 500);
+});
+
+btnStop.addEventListener(
+  'click',
+  () => {
+    clearInterval(timerId);
+    btnStart.setAttribute('disabled', true);
+    btnStart.removeAttribute('disabled');
+  },
+  500
+);
