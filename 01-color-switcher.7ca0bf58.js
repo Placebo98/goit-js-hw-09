d3fe7a1b53f07edd9ddf04e910c5eb10fca71432
Placebo98 <124264544+Placebo98@users.noreply.github.com>
2023-06-25 22:00:20 +0000

    Deploying to gh-pages from @ Placebo98/goit-js-hw-09@356b16bc8619a17c7cd737e2dd233d0fcd4b7eba 🚀

---
+++ 01-color-switcher.7ca0bf58.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),o=null;t.addEventListener("click",(function(){o=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0)),t.setAttribute("disabled",!1)}),500)})),e.addEventListener("click",(function(){clearInterval(o),t.setAttribute("disabled",!0),t.removeAttribute("disabled")}),500)}();
+//# sourceMappingURL=01-color-switcher.7ca0bf58.js.map
