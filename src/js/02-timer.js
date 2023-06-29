1cafb943a4d66c366fea0f6f4a77535394035299
Placebo98 <oleg.tsaryk98@gmail.com>
2023-06-30 01:19:13 +0300

    added changes for hw 9

---
+++ src/js/02-timer.js
@@ -0,0 +1,83 @@
+// Описаний в документації
+import flatpickr from 'flatpickr';
+// Додатковий імпорт стилів
+import 'flatpickr/dist/flatpickr.min.css';
+// all modules
+import Notiflix from 'notiflix';
+
+const timerEl = document.querySelector('.timer');
+const btnStartEl = document.querySelector('[data-start]');
+const daysEl = timerEl.querySelector('[data-days]');
+const hoursEl = timerEl.querySelector('[data-hours]');
+const minutesEl = timerEl.querySelector('[data-minutes]');
+const secondsEl = timerEl.querySelector('[data-seconds]');
+const valueEl = timerEl.querySelectorAll('.value');
+let selectedDate = null;
+
+timerEl.setAttribute('style', 'display: flex; gap: 20px');
+valueEl.forEach(
+  el => ((el.style.display = 'flex'), (el.style.flexDirection = 'column'))
+);
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+    selectedDate = selectedDates[0];
+    if (selectedDate < new Date()) {
+      // window.alert('Please choose a date in the future');
+      Notiflix.Notify.warning('Please choose a date in the future');
+      btnStartEl.setAttribute('disabled', true);
+    } else {
+      btnStartEl.setAttribute('disabled', false);
+      btnStartEl.removeAttribute('disabled');
+    }
+  },
+};
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
+
+function startCount() {
+  countInt = setInterval(() => {
+    const currentTime = new Date().getTime();
+    const differenceOfTime = selectedDate - currentTime;
+    if (differenceOfTime <= 0) {
+      clearInterval(countInt);
+
+      return;
+    }
+    const { days, hours, minutes, seconds } = convertMs(differenceOfTime);
+    daysEl.textContent = addLeadingZero(days);
+    daysEl.textContent = addLeadingZero(days);
+    hoursEl.textContent = addLeadingZero(hours);
+    minutesEl.textContent = addLeadingZero(minutes);
+    secondsEl.textContent = addLeadingZero(seconds);
+  }, 500);
+}
+
+btnStartEl.addEventListener('click', startCount);
+flatpickr('#datetime-picker', options);
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector('.form');
+const delayEl = document.querySelector('input[name="delay"]');
+const stepEl = document.querySelector('input[name="step"]');
+const amountEl = document.querySelector('input[name="amount"]');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function callPromise(event) {
+  event.preventDefault();
+  let delayValue = Number(delayEl.value);
+  const stepValue = Number(stepEl.value);
+  const amountValue = Number(amountEl.value);
+
+  for (let index = 1; index < amountValue; index += 1) {
+    createPromise(index, delayValue)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.warning(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    delayValue += stepValue;
   }
 }
+
+formEl.addEventListener('submit', callPromise);
